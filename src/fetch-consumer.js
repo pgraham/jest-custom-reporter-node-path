21998526c75c7a23a0db230af1e380ae6fbb86ae
philip.graham <philip.graham@accenture.com>
2022-05-20 15:22:08 -0400

    downgrade node-fetch to work as commonJS module

--- src/fetch-consumer.js
+++ src/fetch-consumer.js
@@ -1,6 +1,8 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
 
 async function doFetch() {
-	return await fetch('google.com');
+	const resp = await fetch('https://catfact.ninja/fact')
+	const data = await resp.json()
+	return data
 }
 module.exports = doFetch
